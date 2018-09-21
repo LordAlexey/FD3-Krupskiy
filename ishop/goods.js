8228bb149a2e51ff983294b6f46d2be657bdc95b
LordAlexey <dachadybenci2@gmail.com>
2018-09-21 14:49:44 +0300

    homework1 end

--- ishop/goods.js
+++ ishop/goods.js
@@ -22,11 +22,15 @@ var GoodsBlock = React.createClass({
             goods[key] = React.DOM.div({
                    key:item.code,className:"good col-4"
                 },
-                    React.DOM.h1({className:"name"},item.name),
-                    React.DOM.img({className:"img",src:item.url}),
-                    React.DOM.span({className:"price"},item.price),
-                    React.DOM.span({className:"count"},item.count),
-                
+                  React.DOM.div({className:"wrap"},
+                    React.DOM.h3({className:"name"},item.name),
+                      React.DOM.img({className:"img",src:item.url}),
+                      React.DOM.div({className:"description"},
+                        React.DOM.span({className:"price col-6"},"Цена: " + item.price),
+                        React.DOM.span({className:"count col-6"},"Количество: " + item.count),
+                      )
+                    
+                  )
                 )
             })
 
@@ -35,7 +39,11 @@ var GoodsBlock = React.createClass({
                // goods
                 );
                     
-           return React.DOM.div({className:"container"},React.DOM.div({className:"row"},
+           return React.DOM.div({className:"container"},
+           React.DOM.div({className:"row"},
+           React.DOM.h1({className:"title"}, this.props.category),
+           ),
+           React.DOM.div({className:"row all-goods"},
            goods
       ));
 
